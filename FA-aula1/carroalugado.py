ff5abc558b8c79e7bf8808c45b011afac5c4b360
mariastnfurtado <mstainfurtado@gmail.com>
2026-02-13 20:31:19 -0300

    Add files via upload

---
+++ FA-aula1/carroalugado.py
@@ -0,0 +1,4 @@
+km = int(input("Quantos km percorrido?"))
+dias = int(input("Quantos dias de uso?"))
+valor = (60*dias) + (0.15*km)
+print("O valor a ser pago é %.2f" %valor)
---
+++ FA-aula1/pesoideal.py
@@ -0,0 +1,3 @@
+altura= float(input("Qual é a sua altura?"))
+pesoideal = (72.7*altura)-58
+print("O seu peso ideal é %.2f" % pesoideal)
